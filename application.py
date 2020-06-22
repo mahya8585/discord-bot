5cc5e6385e1fc8ac10040949d86350a784a434e0
maaya <huki.j37@gmail.com>
2020-06-22 19:47:02 +0900

    コメント解除

--- application.py
+++ application.py
@@ -8,33 +8,32 @@ app = Flask(__name__)
 client = discord.Client()
 
 
-# @client.event
-# async def on_message(msg):
-#     """bot動作本体の処理。チャネルにメッセージ送付されたときの処理"""
-#     message = msg.content
-#     if message.startswith('/choice'):
-#         words = message.split()
-#
-#         if (len(words) >= 2) and (words[1].isdecimal()):
-#             # メッセージ第一引数までの数字をランダム選択する
-#             try:
-#                 target = [x for x in range(int(words[1]))]
-#             except:
-#                 await msg.channel.send('エラーが発生しました。第一引数には半角数字(1以上)を入力してください。')
-#
-#             decide = random.choice(target)
-#             await msg.channel.send(str(decide) + 'でおねがいします!!!')
-#
-#         else:
-#             # validation error
-#             await msg.channel.send('いくつまでの数字から選択するのか指定してください。1より大きい数字を入力してください。')
+@client.event
+async def on_message(msg):
+    """bot動作本体の処理。チャネルにメッセージ送付されたときの処理"""
+    message = msg.content
+    if message.startswith('/choice'):
+        words = message.split()
+
+        if (len(words) >= 2) and (words[1].isdecimal()):
+            # メッセージ第一引数までの数字をランダム選択する
+            try:
+                target = [x for x in range(int(words[1]))]
+            except:
+                await msg.channel.send('エラーが発生しました。第一引数には半角数字(1以上)を入力してください。')
+
+            decide = random.choice(target)
+            await msg.channel.send(str(decide) + 'でおねがいします!!!')
+
+        else:
+            # validation error
+            await msg.channel.send('いくつまでの数字から選択するのか指定してください。1より大きい数字を入力してください。')
 
 
 @app.route('/')
 def health():
     """アプリ(bot)の生存確認"""
-    test = os.environ['DISCORD_TOKEN']
-    return 'Bot is alive now. ' + test
+    return 'Bot is alive now. '
 
 
 def run():
